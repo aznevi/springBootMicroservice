2b12efdb2db4aee00f37dee31b3c93c0af093730
serkan <serkan.aznevi>
2017-10-04 15:51:14 +0300

    c

--- consumer/src/main/java/com/example/demo/ConsumerApplication.java
+++ consumer/src/main/java/com/example/demo/ConsumerApplication.java
@@ -1,11 +1,36 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class ConsumerApplication {
 
+	@Autowired
+	private EurekaClient ec;
+	
+	@Autowired
+	private RestTemplate rt;
+	
+	@RequestMapping(method=RequestMethod.GET,path="/produce")
+	public String callProducer(){
+		InstanceInfo instanceInfo = ec.getNextServerFromEureka("producer", false);
+		String homePageUrl = instanceInfo.getHomePageUrl()+"/serverinfo";
+		return rt.getForObject(homePageUrl, String.class);
+		
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
--- producer/src/main/java/com/example/demo/ProducerApplication.java
+++ producer/src/main/java/com/example/demo/ProducerApplication.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class ProducerApplication {
-
+	
+	@Value("${server.port}")
+	private int port;
+	
+	@RequestMapping(method=RequestMethod.GET,path="/serverinfo")
+	public String serverInfo(){
+		return "My port : "+ port;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
 	}
