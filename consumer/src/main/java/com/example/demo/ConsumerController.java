fa883c9d2f87153ac7ef4f8fe26236134a81f990
serkan <serkan.aznevi>
2017-10-04 13:54:51 +0300

    ss

---
+++ consumer/src/main/java/com/example/demo/ConsumerController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController {
+
+    @Value("${my.message}")
+    private String message;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/msg1")
+    public String getMessageText(@Value("${my.message}") final String msg) {
+        return "msg from function : " + msg + "msg from class : " + this.message;
+    }
+
+}
+
